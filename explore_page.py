ee123831445b3e66f0ba91b6124655a81e315335
Pasindu Purnamal <sasindunisal@gmail.com>
2022-11-02 23:12:46 +0530

    Create explore_page.py

---
+++ explore_page.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def show_explore_page():
+    st.title("DIABETES PREDICTION TEST")
+
+    st.write(
+        """
+    ### DIABETES PREDICTION TEST
+    """
+    )
+
+
